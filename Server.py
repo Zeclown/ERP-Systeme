2f6a87ab9971ed09c03b5636f193c2a096d557fd
unknown <0963385@407W23.decinfo.cvm>
2015-09-30 12:30:00 -0400

    adder serveur

---
+++ Server.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import Pyro4
+import socket
+
+class Server(object):
+    def __init__(self):
+        pass
+    
+    def loginValidation(self, user):
+        print("bien recu! "+user)
+        message = "Bonjour "+user+"!"
+        return message
+ 
+    def writeIP(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("gmail.com",80))
+        self.monip=s.getsockname()[0]
+
+        f = open("ip address.txt", "w")
+        print(self.monip)
+        f.write(self.monip)
+        f.close()
+        
+    def correctIP(self):
+        f = open("ip address.txt", "r")
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("gmail.com",80))
+        self.monip=s.getsockname()[0]
+        
+        if(f.readline() != self.monip):
+            writeIP()
+
+serverPyro = Server()   #objet du serveur
+
+daemon = Pyro4.Daemon(host="10.57.47.23",port=43225)      #ce qui écoute les remote calls sur le serveur
+uri = daemon.register(serverPyro,"foo")
+
+serverPyro.writeIP()
+serverPyro.correctIP()
+
+print("ready")
+daemon.requestLoop()
+        
