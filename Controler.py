7b4d7961d4870582cb240052c1afb94cf9ffa763
alexandre <1137793@407W25.decinfo.cvm>
2015-09-30 12:21:26 -0400

    test de alex

---
+++ Controler.py
@@ -0,0 +1,10 @@
+import ServerCommunication
+
+class Controler():
+    def __init__(self):
+        self.serverCommunication = ServerCommunication()
+
+
+
+if __name__ == '__main__':
+    c = Controler()
